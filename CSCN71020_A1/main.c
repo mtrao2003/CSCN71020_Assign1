8bf38c02a176c4989944095d0b29bdf8dbff0a80
Muhammad Rao <mtrao2003@gmail.com>
2023-02-10 20:24:16 -0500

    Muhammad Commit 1

--- CSCN71020_A1/main.c
+++ CSCN71020_A1/main.c
@@ -20,8 +20,9 @@ void main() {
 	{
 	case 1:
 		add();
+	case 2:
+		subtract();
 	}
-
 }
 
 void printWelcomeMenu() {
@@ -44,6 +45,19 @@ void add() {
 	scanf_s("%lf", &num2);
 	printf("Enter the third value:");
 	scanf_s("%lf", &num3);
-	result = num1 + num2+num3;
-	printf("%lf + %lf = %lf\n", num1, num2, result);
+	result = num1 + num2 + num3;
+	printf("%lf + %lf + %lf = %lf\n", num1, num2, num3, result);
 }
+
+void subtract() {
+	double num1, num2, num3, result;
+	printf("Enter the first value:");
+	scanf_s("%lf", &num1);
+	printf("Enter the second value:");
+	scanf_s("%lf", &num2);
+	printf("Enter the third value:");
+	scanf_s("%lf", &num3);
+	result = num1 - num2 - num3;
+	printf("%lf - %lf - %lf = %lf\n", num1, num2, num3, result);
+}
+
